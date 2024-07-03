c4167b78134dc66903a1e1d840368d6e910f2f17
LUIS HENRIQUE LEAL RIES <LHRIES@fspoa.br>
2024-07-03 19:38:49 -0300

    Adicionado componente menu com tratamento do menu mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 import Carroussel from "./components/Carroussel"
+import Menu from "./components/Menu"
 
 function App() {
 
   return (
     <>
-      <div className="w3-top w3-margin-botton">
-        <nav className="w3-bar w3-large w3-black">
-          <a className="w3-bar-item w3-button w3-hide-large w3-hide-medium" >&#9776;</a>
-          <a href="#" className="w3-bar-item w3-button">
-            <i className="fa fa-home w3-xlarge"></i>
-          </a>
-          <a href="#" className="w3-bar-item w3-button w3-hide-small">Menu 1</a>
-          <a href="#" className="w3-bar-item w3-button w3-hide-small">Menu 2</a>
-          <a href="#" className="w3-bar-item w3-button w3-hide-small">Menu 3</a>
-          <a href="#" className="w3-bar-item w3-button w3-right">
-            <i className="fa fa-search w3-xlarge"></i>
-          </a>
-        </nav>
-        <nav id="menu-mobile" className="w3-bar-block w3-large w3-black w3-hide w3-hide-large w3-hide-medium">
-          <a href="#" className="w3-bar-item w3-button">Menu 1</a>
-          <a href="#" className="w3-bar-item w3-button">Menu 2</a>
-          <a href="#" className="w3-bar-item w3-button">Menu 3</a>
-        </nav>
-      </div>
-
-
+      <Menu></Menu>
       <main>
         <div className="w3-container w3-padding-16 w3-margin-top">
           <h1>Minha Petshop</h1>
